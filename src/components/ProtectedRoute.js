3be2473c8fa23b19e260d74a93eca8b87afe0003
antrikshmisri <antrikshmisri@gmail.com>
2022-02-28 00:03:40 +0530

    feat: protected route component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import Navbar from "./Navbar";
+import { Button } from "react-bootstrap";
+import Menu from "./menu/Menu";
+
+const ProtectedRoute = ({ children, isAuthenticated }) => {
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  } else {
+    return (
+        <div id="outer-container" style={{ height: "99%" }}>
+        <Navbar />
+          <Menu>
+            <Button className="btn btn-primary btn-block mt-4 action-btn">
+              Settings
+            </Button>
+            <Button className="btn btn-primary btn-block mt-4 action-btn">
+              Log-Out
+            </Button>
+            <Button className="btn btn-primary btn-block mt-4 action-btn">
+              Help
+            </Button>
+          </Menu>
+          <main id="page-wrap">{children}</main>
+        </div>
+    );
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.firebase.auth.uid ? true : false,
+  };
+};
+
+export default connect(mapStateToProps)(ProtectedRoute);
