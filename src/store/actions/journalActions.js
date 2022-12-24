fd70142eae6fbfd331a31b26c0b7a6ffbaeaf3fe
antrikshmisri <antrikshmisri@gmail.com>
2022-12-25 03:21:32 +0530

    feat: use firestore action util func

--- src/store/actions/journalActions.js
+++ src/store/actions/journalActions.js
@@ -1,34 +1,87 @@
-export const createJournal = (journal) => {
-  return (dispatch, getState, { getFirebase }) => {
-    // add journal to journals collection in a doc with the user's id and append it to the user's journals array
+import { getFirestoreData } from "../../firebase";
 
+export const createJournal = (journal) => {
+  return (dispatch, getState) => {
+    const uid = getState().firebase.auth.uid;
+    const { putData } = getFirestoreData("journals", uid);
+
+    const updatedField = {
+      journals: [journal],
+    };
+
+    putData(updatedField).then(() => {
+      dispatch({ type: "CREATE_JOURNAL_SUCCESS", journal });
+      dispatch(UpdateAllJournals(uid, [journal]));
+      dispatch(GetJournalsAtDate(journal.date, false));
+    });
+  };
+};
+
+export const GetJournalsAtDate = (date, convertDate = true) => {
+  return (dispatch, getState) => {
+    let journalsByDate = getState().journal.journalsByDate;
+    const convertedDate = convertDate ? date.toLocaleDateString("en-GB") : date;
+    const journalFound = convertedDate in journalsByDate;
+
+    if (journalFound) {
+      dispatch({
+        type: "GET_JOURNAL_SUCCESS",
+        currentJournals: journalsByDate[convertedDate],
+      });
+    } else {
+      dispatch({ type: "GET_JOURNAL_ERROR", err: "No journal found" });
+    }
+  };
+};
+
+export const GetAllJournals = (uid, refetch = true) => {
+  return (dispatch, getState) => {
+    if (refetch) {
+      const { fetchData } = getFirestoreData("journals", uid);
+
+      fetchData()
+        .then((data) => {
+          dispatch({
+            type: "GET_ALL_JOURNALS_SUCCESS",
+            journals: data.journals,
+            refetch,
+          });
+        })
+        .catch((err) => {
+          dispatch({ type: "GET_ALL_JOURNALS_ERROR", err });
+        });
+    } else {
+      const journals = getState().journal.journals;
+      dispatch({ type: "GET_ALL_JOURNALS_SUCCESS", journals });
+    }
+  };
+};
+
+export const UpdateAllJournals = (uid, journals) => {
+  return (dispatch, getState) => {
+    const allJournals = getState().journal.journals.concat(journals);
+
+    dispatch({ type: "UPDATE_ALL_JOURNALS", allJournals });
+    dispatch(GetAllJournals(uid, false));
+  };
+};
+
+export const UpdateJournal = (journal) => {
+  return (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
     const firestore = firebase.firestore();
     const uid = getState().firebase.auth.uid;
-    let journals = [];
 
-    firestore
-      .collection("journals")
-      .doc(uid)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          journals = [...doc.data().journals];
-          journals.push(journal);
-        } else {
-          journals.push(journal);
-        }
-      })
-      .then(() => {
-        return firestore.collection("journals").doc(uid).set({
-          journals,
-        });
-      })
-      .then(() => {
-        dispatch({ type: "CREATE_JOURNAL_SUCCESS" });
-      })
-      .catch((err) => {
-        dispatch({ type: "CREATE_JOURNAL_ERROR", err });
-      });
+    const { putData } = getFirestoreData("journals", uid);
+
+    const updatedField = {
+      journals: [journal],
+    };
+
+    putData(updatedField).then(() => {
+      dispatch({ type: "UPDATE_JOURNAL_SUCCESS", journal });
+      dispatch(GetAllJournals(uid, false));
+      dispatch(GetJournalsAtDate(journal.date, false));
+    });
   };
 };
--- src/store/reducers/journalReducer.js
+++ src/store/reducers/journalReducer.js
@@ -2,27 +2,138 @@ import notify from "../../utils/toast";
 
 const initState = {
   journals: [],
+  journalsByDate: {},
 };
 
 const journalReducer = (state = initState, action) => {
   switch (action.type) {
     case "CREATE_JOURNAL_SUCCESS":
+      const { journalsByDate } = state;
+      const {
+        journal: { date },
+      } = action;
+
+      const journalsAtDate = journalsByDate[date] || [];
+
       notify("Journal Created!", "success");
+
       return {
         ...state,
         authMessage: {
           type: "success",
           message: "Journal Created!",
         },
+        journalsByDate: {
+          ...journalsByDate,
+          [date]: [...journalsAtDate, action.journal],
+        },
       };
     case "CREATE_JOURNAL_ERROR":
-      notify("Journal Creation Failed!", "error");
+      notify(action.err || "Error in creating the journal", "error");
       return {
         ...state,
         authMessage: {
           type: "error",
-          message: "Journal Creation Failed!",
+          message: action.err || "Error in creating the journal",
         },
+      };
+    case "GET_JOURNAL_SUCCESS":
+      notify("Journal Retrieved!", "success");
+      return {
+        ...state,
+        currentJournals: action.currentJournals,
+      };
+    case "GET_JOURNAL_ERROR":
+      notify(action.err, "error");
+      return {
+        ...state,
+        authMessage: {
+          type: "error",
+          message: action.err,
+        },
+        currentJournals: null,
+      };
+    case "GET_ALL_JOURNALS_SUCCESS":
+      const { journals } = action;
+      const baseState = {
+        ...state,
+        journals: action.journals,
+      };
+
+      /**
+       * Only rebuild the journalsByDate object if the journals are being refetched
+       * from the database.
+       */
+      if (action.refetch) {
+        let tempJournalsByDate = {};
+
+        journals.forEach((journal) => {
+          const { date } = journal;
+
+          if (date in tempJournalsByDate) {
+            tempJournalsByDate[date].push(journal);
+          } else {
+            tempJournalsByDate[date] = [journal];
+          }
+        });
+
+        return {
+          ...baseState,
+          journalsByDate: tempJournalsByDate,
+        };
+      }
+
+      return {
+        ...state,
+        journals: action.journals,
+      };
+    case "GET_ALL_JOURNALS_ERROR":
+      notify(action.err || "Error retireving all journals", "error");
+      return {
+        ...state,
+        authMessage: {
+          type: "error",
+          message: action.err || "Error retireving all journals",
+        },
+      };
+    case "UPDATE_JOURNAL_SUCCESS":
+      notify("Journal Updated!", "success");
+      const updatedJournal = action.journal;
+      const updatedJournalDate = updatedJournal.date;
+      const journalsDateArray = state.journalsByDate[updatedJournalDate]
+
+      const updatedJournalsDateArray = journalsDateArray.map(journal => {
+        if (journal.id === updatedJournal.id) {
+          return updatedJournal
+        }
+        return journal
+      })
+
+      return {
+        ...state,
+        authMessage: {
+          type: "success",
+          message: "Journal Updated!",
+        },
+        journalsByDate: {
+          ...state.journalsByDate,
+          [updatedJournalDate]: updatedJournalsDateArray
+        }
+      };
+    case "UPDATE_JOURNAL_ERROR":
+      notify(action.err || "Error in updating the journal", "error");
+      return {
+        ...state,
+        authMessage: {
+          type: "error",
+          message: action.err || "Error in updating the journal",
+        },
+      };
+    case "UPDATE_ALL_JOURNALS":
+      notify("All Journals Updated!", "success");
+      return {
+        ...state,
+        journals: action.allJournals,
       };
     default:
       return state;
